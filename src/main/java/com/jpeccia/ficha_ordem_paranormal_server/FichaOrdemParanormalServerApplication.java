d81f7c8c0d7e36af79ee38edf686646309dcd548
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 13:40:49 -0300

    first commit

---
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/FichaOrdemParanormalServerApplication.java
@@ -0,0 +1,13 @@
+package com.jpeccia.ficha_ordem_paranormal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaOrdemParanormalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaOrdemParanormalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpeccia/ficha_ordem_paranormal_server/FichaOrdemParanormalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpeccia.ficha_ordem_paranormal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaOrdemParanormalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
