465a997390ca726ff6dfc0d1af5c20bfe83f6021
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 13:58:10 -0300

    feat: create Attributes Entity

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Attributes/Attributes.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Attributes/Attributes.java
@@ -1,5 +1,14 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.Attributes;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
 public class Attributes {
-    
+    private int agility;
+    private int strength;
+    private int intellect;
+    private int presence;
+    private int vigor;
 }
