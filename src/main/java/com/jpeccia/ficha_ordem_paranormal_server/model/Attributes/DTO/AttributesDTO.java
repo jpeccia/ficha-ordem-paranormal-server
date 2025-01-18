fe833011478fefb884fee8c770b485d36ed5b5f4
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:37:19 -0300

    feat: create AttributesDTO

---
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Attributes/DTO/AttributesDTO.java
@@ -0,0 +1,9 @@
+package com.jpeccia.ficha_ordem_paranormal_server.model.Attributes.DTO;
+
+public record AttributesDTO(
+    int agility,
+    int strength,
+    int intellect,
+    int presence,
+    int vigor
+) {}
