450b7b66f8e07603f7acd15cf0ce6f551a0173c4
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:40:31 -0300

    feat: create SkillDTO

---
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Skill/DTO/SkillDTO.java
@@ -0,0 +1,9 @@
+package com.jpeccia.ficha_ordem_paranormal_server.model.Skill.DTO;
+
+public record SkillDTO(
+    Long id,
+    String name,
+    String description,
+    String baseAttribute,
+    boolean trained
+) {}
