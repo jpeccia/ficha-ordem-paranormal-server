d474e1809407c703636750f362a24c42642cc142
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 13:59:17 -0300

    feat: create DerivedStats entity

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/DerivedStats/DerivedStats.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/DerivedStats/DerivedStats.java
@@ -1,5 +1,14 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.DerivedStats;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
 public class DerivedStats {
-    
+    private int healthPoints;
+    private int sanityPoints;
+    private int effortPoints;
+    private int defense;
+    private int movement;
 }
