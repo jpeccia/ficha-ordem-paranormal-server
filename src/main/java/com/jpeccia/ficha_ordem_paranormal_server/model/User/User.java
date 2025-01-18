0a144608d655c37a38d918041f55873fc47b50c3
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:20:00 -0300

    feat: create docker-compose file

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/User/User.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/User/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
     
     @Id
