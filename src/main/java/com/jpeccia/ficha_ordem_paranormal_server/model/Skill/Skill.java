d2bbc410a689bb1d6d161c0ffe8574f09a82ca07
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:05:45 -0300

    feat: create Skill Entity

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Skill/Skill.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Skill/Skill.java
@@ -1,5 +1,35 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.Skill;
 
+import com.jpeccia.ficha_ordem_paranormal_server.model.CharacterSheet.CharacterSheet;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Skill {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String baseAttribute;
+
+    private Boolean trained;
+
+    @ManyToOne
+    @JoinColumn(name = "character_sheet_id", nullable = false)
+    private CharacterSheet characterSheet;
+
 }
