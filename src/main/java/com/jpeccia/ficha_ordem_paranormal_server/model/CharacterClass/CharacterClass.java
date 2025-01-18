bf3f81e135af491ed9459c8b0ba235a04c7b7c34
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:02:50 -0300

    feat: create CharacterClass entity

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/CharacterClass/CharacterClass.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/CharacterClass/CharacterClass.java
@@ -1,5 +1,32 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.CharacterClass;
 
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CharacterClass {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @ElementCollection
+    private List<String> proficiences;
+
+    @ElementCollection
+    private List<String> startingSkills;
 }
