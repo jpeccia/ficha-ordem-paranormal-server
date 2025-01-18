ede69f7403b5950eca1f7750329c15fb1c46b475
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:08:33 -0300

    feat: create InventoryItem

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/InventoryItem/InventoryItem.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/InventoryItem/InventoryItem.java
@@ -1,5 +1,32 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.InventoryItem;
 
+import com.jpeccia.ficha_ordem_paranormal_server.model.CharacterSheet.CharacterSheet;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InventoryItem {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type; // Ex.: Arma, Equipamento Geral.
+    private int weight;
+
+    @ManyToOne
+    @JoinColumn(name = "character_sheet_id", nullable = false)
+    private CharacterSheet characterSheet;
 }
