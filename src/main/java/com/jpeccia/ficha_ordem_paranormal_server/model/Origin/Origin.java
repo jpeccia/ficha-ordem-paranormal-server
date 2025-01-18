7be099e203cc290f91c7da83e162a8e278774c53
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 14:00:54 -0300

    feat: create Origin Entity

--- src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Origin/Origin.java
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/Origin/Origin.java
@@ -1,5 +1,30 @@
 package com.jpeccia.ficha_ordem_paranormal_server.model.Origin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Origin {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @ElementCollection
+    private List<String> benefits = new ArrayList<>();
 }
