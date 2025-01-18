fcef71f4432a5251475a3f591013b6f59e63e070
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-18 13:51:04 -0300

    feat: create User Entity

---
+++ src/main/java/com/jpeccia/ficha_ordem_paranormal_server/model/User/User.java
@@ -0,0 +1,41 @@
+package com.jpeccia.ficha_ordem_paranormal_server.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jpeccia.ficha_ordem_paranormal_server.model.CharacterSheet.CharacterSheet;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CharacterSheet> characterSheets = new ArrayList<>();
+    
+}
